e238f374c979c2049af6db8364ad2418d12042d8
JPagz95 <jordan@softwebcity.com>
2016-02-20 17:00:19 -0700

    battleship game project

---
+++ random_codes/Battleship.py
@@ -0,0 +1,9 @@
+import numpy as np
+import random as rn
+
+def generate_your_board(board):
+    """asks user to place their battleships on their generated board."""
+    
+
+your_board = np.zeros(shape=(10,10))
+print(your_board)
